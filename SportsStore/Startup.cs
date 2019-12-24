3e784edb2abd55b8eca3e785d321d0f5432809e7
Mashud Ahmed <mashudahmed1980@gmail.com>
2019-12-23 21:12:17 -0500

    Updated all version, Installed EFramework core, & Created db class

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -24,7 +24,7 @@ namespace SportsStore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment()) 
             {
                 app.UseDeveloperExceptionPage();
             }
